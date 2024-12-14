e6e04c4e4fd832ab532fb58526a1f7480a35166c
Akwaji Dan Uke <danakwaji3@gmail.com>
2024-12-14 10:35:49 +0100

    -

--- src/Components/logoCarousel.tsx
+++ src/Components/logoCarousel.tsx
@@ -1,4 +1,4 @@
-import Nissan from "../assets/Images/nissan.jpg";
+import Barclays from "../assets/Images/barclays.png";
 import Toto from "../assets/Images/toto.webp";
 import Toyota from "../assets/Images/toyota.png";
 import Vogue from "../assets/Images/vogue.png";
@@ -11,64 +11,64 @@ const Logos = () => {
     <div className="flex  scroll justify-between">
       <div className="h-[200px] flex caro-cont">
         <img
-          src={Nissan}
+          src={Barclays}
           alt="nissan"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Vodaphone}
           alt="vodaphone"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Toyota}
           alt="toyota"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Olx}
           alt="olx"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Vogue}
           alt="vogue"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Toto}
           alt="toto"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
-          src={Nissan}
+          src={Barclays}
           alt="nissan"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Vodaphone}
           alt="vodaphone"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Toyota}
           alt="toyota"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Olx}
           alt="olx"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Vogue}
           alt="vogue"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
         <img
           src={Toto}
           alt="toto"
-          className="h-full w-[30%] object-contain mr-5"
+          className="h-full w-[70%] object-contain mr-5"
         />
       </div>
     </div>
